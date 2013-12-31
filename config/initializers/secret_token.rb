06e6160b5e8d4a307362d9e7a274bb859fef3842
jforaker <jake@chalkable.com>
2013-12-31 13:32:27 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RivaFlix::Application.config.secret_key_base = 'bb6b7edefd80c32453bda17df1c16db860a38cf96bedf3951bfc62d15d60cf776711b797491642697bbad8fd5e33ea48012e7339f61e8c57c355493a55b06ed4'
